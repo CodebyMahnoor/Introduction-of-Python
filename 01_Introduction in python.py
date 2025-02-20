524502bec38b01ae3a8eb806b66a24dc5241be89
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-02-20 10:02:05 -0800

    first commit

---
+++ 01_Introduction in python.py
@@ -0,0 +1,37 @@
+# > Introduction in Python:
+# python is a way to instruct a computer
+# to perform various tasks.
+
+# > Features of Python:
+# 1. free open source. 
+# 2. It is high level language.
+# 3. Develope by Guido van Rossum.
+# 4. Its is Portable language(window,Mac)
+
+# > Its Uses:
+# . Game develope 
+# . Data visualization
+# . Machine learning
+
+# Installation in .py
+
+# > Module: in python it a functionality in python somebody else use in program Its two type built module(Pre install ex:import os)External module(Needs install ex:import flask).
+# > Pip: in python it is use to install the module.
+# > Repl: in python it is a calculator of python to (Read execuate print loop)
+
+# > Comment: in python iskaa matlab hai kai agr kiu cheez ap apna program mai 
+# nhi dekana charahai tou ios koo comment kar dai
+# . Two type of comment
+# 1.Single comment # sai
+# 2.Multi single quote ''' is sai
+
+
+# len in python:
+a ="Mahnoor"
+print(len(a))
+
+
+
+
+
+
