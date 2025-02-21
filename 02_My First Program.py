24104f91c176abc485026483c0c02a1bbe5f44e2
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-02-20 22:55:18 -0800

    My First program

---
+++ 02_My First Program.py
@@ -0,0 +1,10 @@
+# My First Program
+print("Hello world")
+
+# Multiple line comment
+print('''\tTwinkle, twinkle, \tlittle star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky''')
+
+
