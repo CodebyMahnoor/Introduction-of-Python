75b9a6c097ed0218d501c63207b64914c1f2decb
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-02-22 00:40:07 -0800

    Variable-Datatype

---
+++ 03_Variable&Datatyp.py
@@ -0,0 +1,54 @@
+# > Variable & Datatype:
+# Variable is the name of given memory of location to program.Container store a Value
+# a = 30 # a is variable and 30 is value
+ 
+# > Datatype:
+a = "Mahnoor" #String  koo hamesha double cross ""mai liktai hai 
+b = 12  #Int hamesha counting value
+c = 3.5  #Flaot iskoo decimal valuse yanii point wali value
+d = False #Boolean iskoo True / False mai
+e = None  #None iskoo None means empty
+
+# Type kai sai maloom kartai hai python mai function  kii
+print(type(a))
+
+# Operators in Python:
+# Arithmetic operator: +,-,/,* etc.
+# Assigment operator: =,+=,-=, etc.
+# Comparison operator: ==,!=,>,<=,< etc.
+# Logical operator: And,or,not.
+
+# Additional
+a =5
+b =8
+print(a+b)
+#Substraction
+a =5
+b =10
+print(a-b)
+#Multiplication
+a =2
+b =4
+print(a*b)
+#Division
+a =4
+b =8
+print(a/b)
+
+#Typecasting: yani ek variable datatype koo change kar kai dosrai datatype mai baadaal naa.
+a = "1234" # Jaisai int koo baadla string mai
+print(type(a))
+
+# And : yanii dono value agar true dai gii tou woo true dai gaa
+a =30
+b=2
+print("a and b",a>=b)
+# or :yanii doo no mai sai kiu bhi true hogaa tou woo Answer dai dii gii
+a =30
+b=2
+print("a or b",a>=b)
+# not : Agar tou False hoga tou  true dai gii yanii opposite dai gii
+a =34
+b=80 
+print("a not b",a<=b)
+
