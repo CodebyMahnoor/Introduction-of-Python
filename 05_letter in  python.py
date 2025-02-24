29f7edc044fd2f54ffa438de6f5e56bca466290f
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-02-23 21:53:40 -0800

    Letter in python

---
+++ 05_letter in  python.py
@@ -0,0 +1,8 @@
+letter ='''|Dear Maryam|\n \tAlthough a lot of our day-to-day 
+written communication is by email, text message or social med
+there are many times you will still need to write a  formal to
+apply for your dream job, make a formal complaint, communicate 
+with companies or customers, or even write to your local Member.
+\n\t\t\t\t\t\t\t Thanks!
+'''
+print(letter)
