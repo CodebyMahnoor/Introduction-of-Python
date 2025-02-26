2ee719228c68fe07231a1fc0773175b372670792
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-02-26 01:00:41 -0800

    Input-Function

---
+++ 08_Input function.py
@@ -0,0 +1,16 @@
+# Input Function :
+name =input("enter your name") # This function allows the user to take input from the keyword as a string
+print(name)
+#or agar kiu bhi bhi int daalna hoo ya kiu flaot daal rahai hoo tou iskoo liknai sai pheali ap
+#ko input koo batana parta hai istraha
+
+name =int("Enter your marks")# is taraha yai iskii type bhi int dai gaa agar asa nhi kiya tou yai ap koo iskii kii time string dai gaa balii ap kiu bhi datatype likai
+print(name)
+
+
+a = int(input("enter your number"))
+sq=(a*a)
+print(sq)
+
+a =int(input("enter your number"))
+print(type(a))
