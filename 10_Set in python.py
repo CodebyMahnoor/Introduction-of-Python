fc3d36deb0caa720d6f597156b8399c5d026cd65
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-02-28 23:05:59 -0800

    Set-in-python

---
+++ 10_Set in python.py
@@ -0,0 +1,21 @@
+# Set: is a collection of non repititive element.
+
+b = set() # empty set
+# b.add(1)
+b={1,2,3}# agr sada number add karnai hoo tou -----> set={1,2,3}
+print(b)
+
+
+
+# Operators on set:
+s ={1,2,3,4,5,6}
+print(len(s)) # len maaloom kar kai batayai gaa
+print(s)
+
+
+d ={1,2,3,4,6}
+d.remove(3) # set mai sai kiu bhi value remove kar saktai hai
+print(d)
+
+e ={1,2,3,4,6}
+print(e.pop()) # Is mai kiu bhi ek value hota kardai gaa or bakii remove kardai gaa
