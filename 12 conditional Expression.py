01cd7e3c9782e988effab2bed9d0732b282615ea
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-03-03 07:42:05 -0800

    Conditional-Expression

---
+++ 12 conditional Expression.py
@@ -0,0 +1,21 @@
+# Conditional Expression: (If , else ,and elif in python statement are a multi way 
+# decision taken by our program due to certain condition in our code). 
+
+
+boy =int(input("Enter the age of boy \n"))
+if(boy>18): # agar yai hoo tou yai 
+    print("The boy drive the car")
+else:
+    (boy<17) #warna yai kardoo
+    print("boy are not allow to drive car")
+ # Relational Operators: are used to evaluate condition inside the if statement some example of relational operator are 
+ # Ex : == , >= , <= etc.
+
+ # Logical Operator: and , not , or
+
+b =int(input("Enter your number"))
+if(b>35 and b<56):
+    print("Its work")
+else:
+    print("Its not work")
+
