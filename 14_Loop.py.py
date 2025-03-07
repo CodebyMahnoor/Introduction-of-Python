ade7f17279af264e9e6a55fcd5b2774174be1e12
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-03-07 01:34:15 -0800

    Loop-in-python

---
+++ 14_Loop.py.py
@@ -0,0 +1,72 @@
+# Loop: we want to repeat a set statement in our porgram.
+
+# Type of loop in python:
+# There are two type loop of python.
+# 1.while loop
+# 2.For loop
+
+#while: 
+i = 0
+while (i<10):# Hamesha yaad rakai kai end wala number nhi ayayai gaaa jaisai 1 sai laikar 9 taak number aya gaa 10 nhi ayaa gaa 
+        print(i)
+        i+=1
+
+i = 100
+while (i>1):
+        print(i)
+        i-=1
+
+
+# Practice:
+'''Q1: write a program to print 1 to 50 using a while loop.'''
+i = 1
+while(i<50): 
+    print(i)
+    i+=1
+
+'''Q2: write a program to print the contact of a list using while loops.'''
+fruit =["Mango","Apple","Banana","Garphs"]
+i = 0
+while i<len(fruit):
+    print(fruit[i])
+    i+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
