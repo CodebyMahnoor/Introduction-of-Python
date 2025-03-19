c17204cc923ff8a6ee0a68fc773a8bc649235114
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-03-19 01:47:41 -0700

    Read-Write

---
+++ 22_Read&Write.py
@@ -0,0 +1,22 @@
+# I/O file:
+
+'''I/O file stands for "Input/Output file," meaning a file that is used to transfer data
+either into a computer system (input) or out of it (output) - essentially, a file that
+is being read from or written to by a program'''
+  
+# A file is data stores in a storage device. A python program can talk to the file by reading 
+# content from it and writing ccontent to it.
+
+# Type of files:
+# Text files--->(.txt, .c, etc)-----> open to notepad
+# Binary files---->(.jpg, .dat, etc)-----> yai bhi open hojatii hai lakin error bohot atai hai
+
+# RAM---> volatile(Cannot store)
+# HDD---> Non volatile(Its can be stored)
+
+# Python has a lot of function for reading , updating and deleting files.
+
+# Opening a file: Python has an open() function for opening files. It takes 2 parameter: Function and mode.
+# open("this.txt","r")-----> r = mode of opening(read mode)
+# "this.txt"-----> filename
+# open-----> open is a built in function
