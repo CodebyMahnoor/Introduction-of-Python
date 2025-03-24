8aeb2d1751ffe6e110efd64ed7dadbc3922d3f97
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-03-24 02:57:38 -0700

    R&W-Practice-Question-Tables

---
+++ Q3 practice Tables/R&W practice03.py
@@ -0,0 +1,8 @@
+''' Write a progarm to generate multiplication tables from 2 to 20 and 
+write it to the different files. Place these files in a folder for a
+ 13-year old.'''
+
+for i in range(2,21):
+    with open(f"Tables Muitiplication{i}.txt","w")as f:
+        for j in range(1,11):
+            f.write(f"{i}X{j}={i*j}\n")
