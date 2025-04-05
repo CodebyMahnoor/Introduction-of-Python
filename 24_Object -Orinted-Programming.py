7c8cdace651dc870c0a97e692be459e05be5329a
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-04-05 03:22:49 -0700

    Object-Orinted-Programming

---
+++ 24_Object -Orinted-Programming.py
@@ -0,0 +1,21 @@
+# Object Orinted Programming: Solving a problem by creating objects is one of the most popular approaches in programming.
+#  This is called object oriented programming.
+
+# This concept focuses on using reusable code.--------> Implements DRY principle.
+#DRY-------> Donoted repeat yourself
+#Class:
+# A class is a blueprint for,creating objects.-----> Is koo hamesha PascalCase mai liktai hai
+'''
+PascalCase:
+EmployeeName--------> PascalCase
+
+camelCase:
+inNumeric,inFloat------> camelCase
+
+'''
+# class---------> kai under methods or variable hotai hai
+
+# Object: 
+# An object is an instantiation of a class.when class is defined a template (into) is defined. 
+# Memory is allocated only after object instantiation.Object of a given class can invoke the methods available to it
+# without reveling the implementation details to the user.
--- Q10 practice/R&W practice10.py
+++
@@ -1,3 +0,0 @@
-filename = "practice10.txt"
-with open(filename,"w")as f:
-    f.write("")
