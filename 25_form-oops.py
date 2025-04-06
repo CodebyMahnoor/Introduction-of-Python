4c86233a08d676bba1b73b8c7181a8decbb9aaba
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-04-06 03:28:12 -0700

    25_Form-oops

---
+++ 25_form-oops.py
@@ -0,0 +1,8 @@
+class OrderForm:
+    def printdata(self):
+        print(f"Name is {self.name}")
+        print(f"id is {self.id}")
+MahnoorForm=OrderForm()
+MahnoorForm.name="Maryam"
+MahnoorForm.id="@04"
+MahnoorForm.printdata()
