6236be2a5d0027d68a10235b5bd510651e9d2517
CodebyMahnoor <manokhanofficial15@gmail.com>
2025-04-08 23:12:15 -0700

    Self-Parameter

---
+++ 28_Self-Parameter.py
@@ -0,0 +1,27 @@
+# Self Parameter: Self refers to the instance of the class if is automatically passed with a function call from an object.
+
+# mano.getsalary()---------> here self is mano / equivalent to employee get.salary(mano)
+
+#The function getsalary is defined as:
+class Employee:
+    company="Google"
+    def getsalary(self):
+        print(f"salary is {self.company}  is {self.salary}")
+mano=Employee()
+mano.salary=120000
+mano.getsalary()#Employee.getsalary(mano)
+
+#@staticmethods: Sometimes we need a function that doesn't use the self parameter. we can defined
+# a static method like this:
+class Employee:
+    company="Google"
+    def getsalary(self,signature):
+        print(f"salary is {self.company}  is {self.salary}\n{signature}")
+    @staticmethod
+    def greet():
+        print("Good morning Sir")
+
+mano=Employee()
+mano.salary=120000
+mano.getsalary("Thanks") #Employee.getsalary(mano) 
+mano.greet() # Employee.greet()
